85db0fee1463b79fa7455f4f448cf4ce713d6159
quincyhill <therealquincyhill@gmail.com>
2019-08-04 22:10:41 -0500

    did some testing here

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
-const person = {
-  firstName: "Steve",
-  lastName: "Smith",
-  age: 30,
-  email: "steve@aol.com",
-  hobbies: ["music", "sports"],
-  address: {
-    city: "Miami",
-    state: "Florida"
-  },
-  getBirthYear: function() {
-    return new Date().getFullYear() - this.age;
-  }
-};
+// const person = {
+//   firstName: "Steve",
+//   lastName: "Smith",
+//   age: 30,
+//   email: "steve@aol.com",
+//   hobbies: ["music", "sports"],
+//   address: {
+//     city: "Miami",
+//     state: "Florida"
+//   },
+//   getBirthYear: function() {
+//     return new Date().getFullYear() - this.age;
+//   }
+// };
 
-let val;
+// let val;
 
-val = person.lastName;
-val = person["lastName"];
-val = person.age;
-val = person.email;
-val = person.hobbies;
-val = person.hobbies[0];
-val = person.address;
-val = person.address.city;
-val = person.getBirthYear();
+// val = person.lastName;
+// val = person["lastName"];
+// val = person.age;
+// val = person.email;
+// val = person.hobbies;
+// val = person.hobbies[0];
+// val = person.address;
+// val = person.address.city;
+// val = person.getBirthYear();
 
-console.log(val);
+// console.log(val);
 
-const people = [
-  { name: "John", age: 30 },
-  { name: "Mike", age: 29 },
-  { name: "Jason", age: 22 }
-];
+// const people = [
+//   { name: "John", age: 30 },
+//   { name: "Mike", age: 29 },
+//   { name: "Jason", age: 22 }
+// ];
 
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i]);
-}
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
+
+// const doubleIt = entry => {
+//   return entry * 2;
+// };
+
+// console.log(doubleIt(3));
+
+// const doubleIt = function(entry) {
+//   return entry * 2;
+// };
+
+// console.log(doubleIt(5));
+
+// const human = {
+//   name: "Jason",
+//   age: 30,
+//   occupation: "Driver",
+//   isMarried: false,
+//   address: {
+//     state: "IL",
+//     county: "Lake"
+//   },
+//   getBirthYear: function() {
+//     return new Date().getFullYear() - this.age;
+//   }
+// };
+
+// console.log(human.getBirthYear());
